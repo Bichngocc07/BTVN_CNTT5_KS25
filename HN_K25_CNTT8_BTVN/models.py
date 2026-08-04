255b8bd6390db660ef41f67d47593c93e523c10a
Hà Bích Ngọc <ngoc01072006@gmail.com>
2026-08-04 19:34:08 +0700

    Enhance StudentModel with additional fields
    Added student_code, age, and is_active fields to StudentModel. Updated password field to be nullable.

--- HN_K25_CNTT8_BTVN/models.py
+++ HN_K25_CNTT8_BTVN/models.py
@@ -1,11 +1,14 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
 class StudentModel(Base):
     __tablename__ = "students"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    student_code = Column(String(50), unique=True, nullable=False, index=True)
     student_name = Column(String(100), nullable=False)
     email = Column(String(150), unique=True, nullable=False, index=True)
-    password = Column(String(255), nullable=False)  # Mật khẩu
+    password = Column(String(255), nullable=True)
     phone_number = Column(String(20), nullable=True)
+    age = Column(Integer, nullable=True)
+    is_active = Column(Boolean, default=True)
